cebe39283ec733103a955705a66ba7d5d6d1adcb
SamuelD005 <samuel.dodet3@gmail.com>
2021-05-14 11:14:44 +0200

    final csv 5

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ drop_row_without_value("Price",data)
 del data['Surface area of the plot of land']
 del data['Unnamed: 0']
 del data['Url']
-data.drop(data.index[np.where(data.Price > 10000000)[0]])
-data.drop(data.index[np.where(data.Price < 50000)[0]])
+data = data.drop(data.index[np.where(data["Subtype of property"] == "building")[0]])
+
+
 data['Surface of the land'] = data['Area'] + data['Terrace Area'] + data['Garden Area']
 
 
@@ -22,7 +23,7 @@ data["Region"] = data.apply(lambda x: change_to_province(x["Locality"])[1], axis
 data_final = data.reset_index()
 
 print(data.info())
-data_final.to_csv("DataFinal3.csv")
+data_final.to_csv("DataFinal5.csv")
 
 
 
